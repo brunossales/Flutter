ede326c313c40a4a379987686db2a83607a39619
brunossales <bruno.particular25@alu.ufc.br>
2021-08-31 21:07:08 -0300

    adicionando as mudanças e o código da aula 2.4

---
+++ stateless_stateful/README.md
@@ -0,0 +1,9 @@
+# Aula 2.4
+
+## stateless X stateful - widgets
+
+### Aprendi sobre o stateless que não podem ter seus valores modificados;
+
+### Aprendi sobre o stateful que os valores de suas variáveis podem ser modificados;
+
+### E na prática aprendi sobre o late, que é dizer que o construtor de uma váriavel mais vir logo após, late significa tardio.
---
+++ stateless_stateful/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Usuario {
+  late String nome;
+  late String email;
+  late String funcao;
+  late String telefone;
+}
+
+class perfilUsuario extends StatelessWidget {
+  perfilUsuario(this.usuario);
+
+  final Usuario usuario;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+
+}
+class testeState extends StatefulWidget {
+ _testeWidget createState() => _testeWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+
+}
+class _testeWidget extends State<testeState>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+}
+
